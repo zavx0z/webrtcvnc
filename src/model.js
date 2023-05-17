c5f953c60c35588dc9e63c3878bf81fac12deabf
zavx0z <metaversebdfl@gmail.com>
2023-05-17 20:26:08 +0300

    log middleware

--- src/model.js
+++ src/model.js
@@ -1,20 +1,30 @@
-import {flow, types} from "mobx-state-tree"
+import {addMiddleware, flow, types} from "mobx-state-tree"
 import {io} from "socket.io-client"
 import freeice from "freeice"
 
-const sio = io("ws://127.0.0.1:8000", {transports: ["websocket"]}).on("connect", () => console.log("connected"))
+const logMiddleware = (call, next) => {
+    const moduleName = 'RTC'
+    switch (call.name) {
+        case 'changeStatusConnection':
+            console.log(moduleName, call.name, call.args[0])
+            break
+        default:
+            break
+    }
+    next(call)
+}
 
+const sio = io("ws://127.0.0.1:8000", {transports: ["websocket"]}).on("connect", () => console.log("connected"))
 const eventIceCandidate = (event) => {
     console.log('eventIceCandidate', event)
     if (event.candidate)
         sio.emit('new-ice-candidate', event.candidate)
 }
+
 const eventIceGatheringStateChange = (event) => {
     console.log('eventIceGatheringStateChange', event)
 }
-const eventConnectionStateChange = (event) => {
-    console.log('eventConnectionStateChange', event)
-}
+
 const eventDataChannel = (event) => {
     console.log('eventConnectionStateChange', event)
 }
@@ -22,9 +32,19 @@ const eventDataChannel = (event) => {
 const eventDataChannelOpen = (event) => {
     console.log('eventDataChannelOpen', event)
 }
-
 const RTCmodel = types
     .model('RTC', {})
+    .volatile(self => ({
+        connection: types.optional(types.enumeration('connectionstatechange', [
+            'new',
+            'connected',
+            'disconnected',
+            'checking',
+            'closed',
+            'failed',
+            'connecting',
+        ]), 'new')
+    }))
     .actions(self => {
         let peerConnection
         let dataChannel
@@ -40,8 +60,13 @@ const RTCmodel = types
             const remoteDesc = new RTCSessionDescription(answer)
             await peerConnection.setRemoteDescription(remoteDesc)
         }
+        const eventConnectionStateChange = (event) => self['changeStatusConnection'](event.target['connectionState'])
         return {
+            changeStatusConnection(status) {
+                self.connection = status
+            },
             afterCreate() {
+                addMiddleware(self, logMiddleware)
                 peerConnection = new RTCPeerConnection({iceServers: freeice()})
                 peerConnection.addEventListener('icecandidate', eventIceCandidate)
                 peerConnection.addEventListener("icegatheringstatechange", eventIceGatheringStateChange)
