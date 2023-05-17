287aacc098bc577b3a08385357b35fe08ee046eb
zavx0z <metaversebdfl@gmail.com>
2023-05-17 18:47:08 +0300

    new-ice-candidate

--- src/model.js
+++ src/model.js
@@ -3,37 +3,59 @@ import {io} from "socket.io-client"
 import freeice from "freeice"
 
 const sio = io("ws://127.0.0.1:8000", {transports: ["websocket"]}).on("connect", () => console.log("connected"))
+
+const eventIceCandidate = (event) => {
+    console.log('eventIceCandidate', event)
+    if (event.candidate)
+        sio.emit('new-ice-candidate', event.candidate)
+}
+const eventIceGatheringStateChange = (event) => {
+    console.log('eventIceGatheringStateChange', event)
+}
+const eventConnectionStateChange = (event) => {
+    console.log('eventConnectionStateChange', event)
+}
+const eventDataChannel = (event) => {
+    console.log('eventConnectionStateChange', event)
+}
+
+const eventDataChannelOpen = (event) => {
+    console.log('eventDataChannelOpen', event)
+}
+
 const RTCmodel = types
     .model('RTC', {})
     .actions(self => {
         let peerConnection
         let dataChannel
 
-        const sendOffer = async () => {
+        const messageSendOffer = async () => {
             console.log('sendOffer')
             const offer = await peerConnection.createOffer()
             await peerConnection.setLocalDescription(offer)
             sio.emit('offer', offer)
         }
-        const receiveAnswer = async ({answer, sender}) => {
+        const messageReceiveAnswer = async ({answer, sender}) => {
             console.log(answer.type, sender, answer)
             const remoteDesc = new RTCSessionDescription(answer)
             await peerConnection.setRemoteDescription(remoteDesc)
-            console.log(peerConnection.iceGatheringState)
         }
         return {
             afterCreate() {
                 peerConnection = new RTCPeerConnection({iceServers: freeice()})
+                peerConnection.addEventListener('icecandidate', eventIceCandidate)
+                peerConnection.addEventListener("icegatheringstatechange", eventIceGatheringStateChange)
+                peerConnection.addEventListener('connectionstatechange', eventConnectionStateChange)
+                peerConnection.addEventListener('datachannel', eventDataChannel)
+
                 dataChannel = peerConnection.createDataChannel('data')
-                dataChannel.addEventListener('open', console.log)
-                peerConnection.addEventListener('icecandidate', console.log)
-                peerConnection.addEventListener("icegatheringstatechange", console.log)
-                peerConnection.addEventListener('connectionstatechange', console.log)
-                sio.on('answer', receiveAnswer)
+                dataChannel.addEventListener('open', eventDataChannelOpen)
+
+                sio.on('answer', messageReceiveAnswer)
             },
             start: flow(function* () {
                 try {
-                    yield sendOffer()
+                    yield messageSendOffer()
                 } catch (e) {
                     return Promise.reject(e)
                 }
